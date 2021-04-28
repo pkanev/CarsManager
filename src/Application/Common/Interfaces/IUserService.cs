d0edddb51085b4c403cd2685e2c90754cfdaba2e
Petar Kanev <pvkanev@gmail.com>
2021-04-28 19:43:19 +0300

    refactoring

--- src/Application/Common/Interfaces/IUserService.cs
+++ src/Application/Common/Interfaces/IUserService.cs
@@ -5,12 +5,6 @@ namespace CarsManager.Application.Common.Interfaces
 {
     public interface IUserService
     {
-        //User Authenticate(string username, string password);
-        //Task<IList<User>> GetAllAsync();
-        //Task<User> CreateAsync(User user, string password);
-        //Task UpdateAsync(User user, string password = null);
-        //Task DeleteAsync(int id);
-        //Task AssignRoleAsync(string userId, string role);
         Task<User> GetByIdAsync(string id);
         Task<string> GetUserNameAsync(string id);
         Task<bool> IsInRoleAsync(string userId, string role);
