19f6d4030325a010b75f9ea314856ee851a7a613
Petar Kanev <pvkanev@gmail.com>
2021-05-06 15:10:57 +0300

    update license plate regex

--- src/Application/Common/Constants/VehicleConstants.cs
+++ src/Application/Common/Constants/VehicleConstants.cs
@@ -2,6 +2,6 @@
 {
     public static class VehicleConstants
     {
-        public const string LICENSE_PLATE_REGEX = @"^[A-Z]{1,2}[0-9]{4}[A-Z]{1,2}$";
+        public const string LICENSE_PLATE_REGEX = @"^[ETYOPAHKXCBMВЕРТОАСХКНМ]{1,2}[0-9]{4}[ETYOPAHKXCBMВЕРТОАСХКНМ]{1,2}$";
     }
 }
